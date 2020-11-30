c0ede5a82451f6c0454d61bce08c4769ad39bc07
oucifkarim01 <oucifkarim01@gmail.com>
2020-11-30 09:13:27 +0100

    netlify redirects

--- src/src/Product/Product3D.js
+++ src/src/Product/Product3D.js
@@ -12,6 +12,12 @@ function Product3D() {
           autoplay
           className="3d_viewer"
         ></model-viewer>
+        <a
+          href="intent://arvr.google.com/scene-viewer/1.0?file=
+          https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;"
+        >
+          Avocado
+        </a>
       </div>
     </div>
   );
