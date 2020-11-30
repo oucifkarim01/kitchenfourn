1d4d770f487645c022b2ce92acf5141e9fdd241c
oucifkarim01 <oucifkarim01@gmail.com>
2020-11-30 09:17:21 +0100

    netlify redirects

--- src/src/Product/Product3D.js
+++ src/src/Product/Product3D.js
@@ -1,12 +1,10 @@
 function Product3D() {
   return (
     <div className="fluid-container viewer_blocck">
-      <a
-        href="intent://arvr.google.com/scene-viewer/1.0?file=
-          https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;"
-      >
-        View Space
+      <a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf&mode=3d_preferred#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">
+        Avocado
       </a>
+      ;
       <div className="container" style={{ width: "100%", height: "500px" }}>
         <model-viewer
           src={`${process.env.PUBLIC_URL}/assets/models/Horse.glb`}
